73091e5c9a632a417172c9cca889d6c1800ded94
DanielCardozoH13 <cardozodaniel36@gmail.com>
2019-11-14 13:24:10 -0500

    updated model perfil

--- instagram/perfiles/models.py
+++ instagram/perfiles/models.py
@@ -1,14 +1,20 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
 class Perfile(models.Model):
+	user = models. OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
 	biografia = models.TextField(max_length=1000, blank = True, null =True)
 	sitio_web = models.CharField(max_length=45, blank=True, null = True)
 	sexo = models.PositiveIntegerField(blank=True, null=True)
+	telefono = models.CharField(blank=True, max_length=20, null=True)
+	created = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+	modified = models.DateTimeField(auto_now=True, blank=True, null = True)
 	foto_perfil = models.ImageField(upload_to='perfiles/fotos', 
 		blank=True,
 		null=True,
 		)
+
 
 class Foto(models.Model):
 	created = models.DateTimeField(null=False, blank=False)
