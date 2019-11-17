e0e432ae82db1ca267da0d5de3ff08300734c7dd
afurourrego <infoafuro@gmail.com>
2019-11-17 12:21:38 -0500

    work

--- instagram/perfiles/models.py
+++ instagram/perfiles/models.py
@@ -3,14 +3,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Perfil(models.Model):
-	user = models. OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
+	user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
 	biografia = models.TextField(max_length=1000, blank = True, null =True)
 	sitio_web = models.CharField(max_length=45, blank=True, null = True)
 	sexo = models.PositiveIntegerField(blank=True, null=True)
 	telefono = models.CharField(blank=True, max_length=20, null=True)
 	created = models.DateTimeField(auto_now_add=True, blank=True, null=True)
 	modified = models.DateTimeField(auto_now=True, blank=True, null = True)
-	foto_perfil = models.ImageField(upload_to='perfiles/fotos', 
+	foto_perfil = models.ImageField(upload_to='perfiles/fotos',
 		blank=True,
 		null=True,
 		)
@@ -21,7 +21,7 @@ class Perfil(models.Model):
 class Foto(models.Model):
 	perfil_id = models.ForeignKey(Perfil, on_delete=models.CASCADE, blank=True, null=True)
 	titulo = models.TextField(max_length=1000, blank=False, null=True)
-	foto = models.ImageField(upload_to='perfiles/fotos', 
+	foto = models.ImageField(upload_to='perfiles/fotos',
 		blank=False,
 		null=False,
 		)
--- instagram/perfiles/views.py
+++ instagram/perfiles/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm 
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
-
+from perfiles.models import Perfil
 
 def logout_view(request):
 	return render(request, 'perfiles/login.html')
@@ -22,3 +23,33 @@ def logup_view(request):
 		form = UserCreationForm()
 
 	return render(request,'registration/logup.html', {'form':form})
+
+@login_required
+def update_profile(request):
+	profile = request.user.perfil
+
+	if request.method == 'POST':
+		form = PerfilForm(request.POST, request.FILES)
+		if form.is_valid():
+			data = form.cleaned_data
+			profile.biografia = data['biografia']
+			profile.sitio_web = data['sitio_web']
+			profile.sexo = data['sexo']
+			profile.telefono = data['telefono']
+			profile.foto_perfil = data['foto_perfil']
+			profile.save()
+
+			return redirect('update_profile')
+
+	else:
+		form = PerfilForm()
+
+	return render(
+		request=request,
+		template_name = 'users/update_profile.html',
+		context = {
+			'profile': profile,
+			'user':	request.user,
+			'form' : form
+			}
+		)
