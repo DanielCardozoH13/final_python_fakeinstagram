244f1010078fc0b438c7aa1ed158e5c1c02f9c20
afurourrego <infoafuro@gmail.com>
2019-11-28 02:31:08 -0500

    historia

--- instagram/static/js/init.js
+++ instagram/static/js/init.js
@@ -7,5 +7,18 @@
       $('.modal').modal();
     });
 
+    $(document).ready(function() {
+      var elems = document.querySelectorAll('.modal');
+      var instances = M.Modal.init(elems, {
+     		'onOpenEnd': initCarouselModal
+     });
+
+    function initCarouselModal() {
+      var elems = document.querySelectorAll('.carousel');
+      var instances = M.Carousel.init(elems, {'fullWidth': true});
+      instances[0].set(2);
+    }
+  });
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
